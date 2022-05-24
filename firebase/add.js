684128fb2452defebc18bd8c9b9a8afb619a8efa
rajafss <rajafss440@gmail.com>
2022-05-24 09:06:05 +0100

    firebase hosting

---
+++ firebase/add.js
@@ -0,0 +1,21 @@
+// const name = document.getElementById("fname");
+// const category_id = document.getElementById("category_id");
+// const name_ar = document.getElementById("name_ar");
+// const name_en = document.getElementById("name_en");
+// const price = document.getElementById("price");
+// const quantity = document.getElementById("quantity");
+
+
+// const addbtn = document.getElementById("add");
+
+// const database =  firebase.database();
+
+// addbtn.addEventListener('Click' , (e) => {
+//     e.preventDefault();
+//     database.ref('/services/' + category_id.value).set({
+
+//     })
+
+// });
+
+
